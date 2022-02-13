18c7b5f4a1f1726ee1ce545f64136dfb6b657a4d
nayarasaffa <95131818+nayarasaffa@users.noreply.github.com>
2022-02-13 17:46:14 +0700

    Update ConvertDataType.java

--- Operators/ConvertDataType.java
+++ Operators/ConvertDataType.java
@@ -1,11 +1,33 @@
 package Operators;
 
+/**
+ * <h1>Soal 3: Convert data type</h1>
+ * Convert data type program implements an application
+ * to convert large data types to small data types
+ * (long -> int -> short) and (double -> float -> byte)
+ * 
+ * @author Nayara Saffa
+ * @version 1.0
+ * @since 2022-02-12
+ */
+
 public class ConvertDataType {
+	/**
+	 * This method is used to convert number
+	 * from long to int to short
+	 * @param l (the number to be converted)
+	 */
 	static short methodOne(long l)
 	 {
 	 int i = (int) l;
 	 return (short)i; 
 	 }
+	/**
+	 * This is the main method
+	 * Here's where the number that have large data types 
+	 * are converted to small data types
+	 * @param args
+	 */
 	 public static void main(String[] args)
 	 {
 	 double d = 10.25;
