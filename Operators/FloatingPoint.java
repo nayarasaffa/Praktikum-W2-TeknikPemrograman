2da157301402904792a3150e47e9ff684ae639eb
nayarasaffa <95131818+nayarasaffa@users.noreply.github.com>
2022-02-13 18:09:48 +0700

    Update FloatingPoint.java

--- Operators/FloatingPoint.java
+++ Operators/FloatingPoint.java
@@ -1,6 +1,21 @@
-package Operators;
+package FloatingPoint;
+
+/**
+ * <h1>Soal 4 : Floating Point</h1>
+ * Floating point program implements an application
+ * to rounding a number to the nearest integer
+ * 
+ * @author Nayara Saffa
+ * @version 1.0
+ * @since 2022-02-12
+ */
 
 public class FloatingPoint {
+	/**
+	 * This is the main method.
+	 * Here's where the number is 
+	 * rounded to the nearest integer
+	 */
 	public static void main(String[] args) {
 		 double x = 92.98; 
 		 int nx = (int) Math.round(x);
