9fbed6c141fa4acbfb5961f41bed2bd9001b6d86
Nayara Saffa <95131818+nayarasaffa@users.noreply.github.com>
2022-02-13 22:02:01 +0700

    Update Strings.java

--- Strings/Strings.java
+++ Strings/Strings.java
@@ -32,15 +32,6 @@ public class Strings {
             } else {
                 System.out.println("no");
             }
-            // Stringname.substring(starting index,end of the index).toUpperCase() will add
-            // uppercase
-            // from starting index until the end of index.
-            // Stringname.substring(index) will remove a character of index in a string.
-            // if A.substring(1, 1).toUpperCase() is used instead it will just remove the
-            // first char of string.
-            // if A.substring(2) instead it will remove 2 character from initial string
-            // before toUpperCase().
-            // if only A.substring(0, 1).toUpperCase() it will become just uppercase of string A index 1.
             System.out.printf("%s %s", A.substring(0, 1).toUpperCase() + A.substring(1),
                     B.substring(0, 1).toUpperCase() + B.substring(1));
         } catch (Exception e) {
