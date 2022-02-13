5c55c1ee1a489e0e100dbc46f3734b1f2a3f7fc2
nayarasaffa <95131818+nayarasaffa@users.noreply.github.com>
2022-02-13 17:47:16 +0700

    Update Constants2.java

--- Variable/Constants2.java
+++ Variable/Constants2.java
@@ -1,11 +1,27 @@
-package Constants;
+package Variable;
+
+/**
+ * <h1>Soal 3: Constants</h1>
+ * Constants program implements an application
+ * to convert a paper size from inches to centimeters
+ * 
+ * @author Nayara Saffa
+ * @version 1.0
+ * @since 2022-02-12
+ */
 
 public class Constants2 {
-	public static final double CM_PER_INCH = 2.54; 
+	/**
+	 * This is the main method.
+	 * Here's where the number of inches is 
+	 * multiplied by centimeters per inch
+	 * and turns into centimeters
+	 */
+	public static final double CM_PER_INCH = 2.54;
 	 public static void main(String[] args) {
 	 double paperWidth = 8.5; 
 	 double paperHeight = 11; 
 	 System.out.println("Paper size in centimeters: " + paperWidth *
 	 CM_PER_INCH + " by " + paperHeight * CM_PER_INCH); 
-	 }
+	 } 
 }
